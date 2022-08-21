9bed30e7210c38fb228651945e9b52984f083236
Bkortun <bariskorun@gmail.com>
2022-08-21 20:05:54 +0300

    admin layout eklendi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { LayoutComponent } from './admin/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admin",component:LayoutComponent,children:[
+    {path:"",component:DashboardComponent}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
