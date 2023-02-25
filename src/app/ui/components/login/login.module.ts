14cac9262cb7932a17f5fde05c9310fb70aa08aa
Baris Kortun <bariskorun@gmail.com>
2023-02-25 21:17:50 +0300

    Login page was added

--- src/app/ui/components/login/login.module.ts
+++ src/app/ui/components/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -12,6 +13,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule.forChild([{path:"",component:LoginComponent}]),
+    FormsModule,
+    ReactiveFormsModule //html'deki [formGroup]="loginForm"'nin çalışması için lazım
   ]
 })
 export class LoginModule { }
