dad20b9562bc2e5a5b0bf14aa8502b45bb6f9a13
Baris Kortun <bariskorun@gmail.com>
2023-04-01 06:56:18 +0300

    Address part was added

--- src/app/ui/components/basket/basket.component.ts
+++ src/app/ui/components/basket/basket.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserAddress } from 'app/contracts/address/user_address';
+import { AddressService } from 'app/services/common/modals/address.service';
 import { AuthService } from 'app/services/common/modals/auth.service';
 
 @Component({
@@ -8,15 +11,34 @@ import { AuthService } from 'app/services/common/modals/auth.service';
 })
 export class BasketComponent implements OnInit {
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService:AuthService,private addressService:AddressService
+    ,private activatedRoute:ActivatedRoute) { }
 
   userId:string;
+  userAddresses:UserAddress[];
+  selectedAddress:UserAddress;
 
   ngOnInit(): void {
     this.userId=this.getUserId();
+    this.getAddresses();
   }
 
   getUserId(){
     return this.authService.decodeToken().nameIdentifier;
   }
+
+  async getAddresses() {
+    const list = await this.addressService.getAddresses(this.activatedRoute.snapshot.paramMap.get("userId"));
+    console.log(list.items)
+    this.userAddresses =list.items;
+  }
+
+  getAddressId(id: string) {
+    this.userAddresses.forEach(userAddress => {
+      if (userAddress.addressId == id)
+        this.selectedAddress = userAddress;
+    });
+    console.log(this.selectedAddress)
+  }
+
 }
